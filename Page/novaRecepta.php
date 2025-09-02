5df5357cf15f02c90573a93913415e6ab37dbc91
Marçal Tineo Estevez <50370914+MarcalTineo@users.noreply.github.com>
2025-09-02 22:06:35 +0200

    sd

--- Page/novaRecepta.php
+++ Page/novaRecepta.php
@@ -24,85 +24,128 @@
       ?>
       <main>
          <div id="content">
-            <div id="nr_wrapper">
-               <div id="nr_title">
-                  <h2>Nova Recepta</h2>
-               </div>
-               <div class="nr_input" id="nr_title_field">
-                  <div class="nr_input_title">
-                     <label for="nr_title">Títol de la Recepta</label>
+            <form action="ProcessNewRecipe.php" method="post">
+               <div id="nr_wrapper">
+                  <div id="nr_title">
+                     <h2>Nova Recepta</h2>
                   </div>
-                  <div class="nr_input_field">
-                     <input type="text" name="nr_title" id="nr_titleRecepta" placeholder="Títol de la Recepta">
+                  <div class="nr_input" id="nr_title_field">
+                     <div class="nr_input_title">
+                        <label for="nr_title">Títol de la Recepta</label>
+                     </div>
+                     <div class="nr_input_field">
+                        <input type="text" name="nr_title" id="nr_titleRecepta" placeholder="Títol de la Recepta">
+                     </div>
                   </div>
-               </div>
-               <div class="nr_input" id="nr_category_field">
-                  <div class="nr_input_title">
-                     <label for="nr_category">Categoria</label>
-                  </div>
-                  <div class="nr_input_field">
-                     <select name="nr_category" id="nr_category">
-                        <option value="" disabled selected>Categories</option>
-                        <?php
-                        $categories = Category::readAll();
-                        foreach ($categories as $key => $value) {
-                           ?>
-                           <option value="<?php echo $value['name'] ?>"><?php echo $value['name'] ?></option>
+                  <div class="nr_input" id="nr_category_field">
+                     <div class="nr_input_title">
+                        <label for="nr_category">Categoria</label>
+                     </div>
+                     <div class="nr_input_field">
+                        <select name="nr_category" id="nr_category">
+                           <option value="" disabled selected>Categories</option>
                            <?php
-                        }
-                        ?>
-                     </select>
-                     <!-- <input type="text" name="nr_category" id="nr_category" placeholder="Categoria"> -->
+                           $categories = Category::readAll();
+                           foreach ($categories as $key => $value) {
+                              ?>
+                              <option value="<?php echo $value['name'] ?>"><?php echo $value['name'] ?></option>
+                              <?php
+                           }
+                           ?>
+                        </select>
+                     </div>
                   </div>
-               </div>
-               <div class="nr_input" id="nr_dificulty_field">
-                  <div class="nr_input_title">
-                     <label for="nr_title">Dificultat</label>
+                  <div class="nr_input" id="nr_dificulty_field">
+                     <div class="nr_input_title">
+                        <label for="nr_title">Dificultat</label>
+                     </div>
+                     <div class="nr_input_select">
+                        <div class="nr_tag">
+                           <p>Fàcil</p>
+                        </div>
+                        <div class="nr_tag">
+                           <p>Mitjà</p>
+                        </div>
+                        <div class="nr_tag">
+                           <p>Difícil</p>
+                        </div>
+                        <p class="nr_input_selection"></p>
+                     </div>
                   </div>
-                  <div class="nr_input_select">
-                     <div class="nr_tag">
-                        <p>Fàcil</p>
+                  <div class="nr_input" id="nr_dificulty_field">
+                     <div class="nr_input_title">
+                        <label for="nr_title">Racions</label>
                      </div>
-                     <div class="nr_tag">
-                        <p>Mitjà</p>
+                     <div class="nr_input_select">
+                        <div class="nr_tag ">
+                           <p>1</p>
+                        </div>
+                        <div class="nr_tag">
+                           <p>2</p>
+                        </div>
+                        <div class="nr_tag">
+                           <p>4</p>
+                        </div>
+                        <div class="nr_tag">
+                           <p>6</p>
+                        </div>
+                        <div class="nr_tag">
+                           <p>8</p>
+                        </div>
+                        <div class="nr_tag">
+                           <p>10</p>
+                        </div>
+                        <p class="nr_input_selection"></p>
                      </div>
-                     <div class="nr_tag">
-                        <p>Difícil</p>
-                     </div>
-                     <p class="nr_input_selection"></p>
                   </div>
-               </div>
-               <div class="nr_input" id="nr_dificulty_field">
-                  <div class="nr_input_title">
-                     <label for="nr_title">Racions</label>
+                  <div class="nr_input" id="nr_description_field">
+                     <div class="nr_input_title">
+                        <label for="nr_description">Descripció breu</label>
+                     </div>
+                     <div class="nr_input_field">
+                        <textarea name="nr_description" id="nr_description" rows="6"></textarea>
+                        <!-- <input type="textarea" name="nr_description" id="nr_description" placeholder="Descripció breu"
+                        rows="5"> -->
+                     </div>
                   </div>
-                  <div class="nr_input_select">
-                     <div class="nr_tag ">
-                        <p>1</p>
+                  <h3 id="nr_section_ingredients">Ingredients</h3>
+
+                  <div id="nr_ingredients_wrapper">
+                     <div class="nr_input" id="nr_ingredient_search">
+                        <div class="nr_input_title">
+                           <label>Busca ingredients</label>
+                        </div>
+                        <div class="nr_input_field">
+                           <input type="text" name="" id="nr_ingredient_search_box" placeholder="Busca ingredients">
+                        </div>
                      </div>
-                     <div class="nr_tag">
-                        <p>2</p>
+                     <div class="nr_input_sugestions" id="nr_ingredient_suggestions">
+                        <ul>
+
+                           <li>Patata</li>
+                           <li>Patata</li>
+                           <li>Patata</li>
+                           <li>Patata</li>
+                           <li>Patata</li>
+                           <li>Patata</li>
+                           <li>Patata</li>
+                           <li>Patata</li>
+                        </ul>
                      </div>
-                     <div class="nr_tag">
-                        <p>4</p>
+                     <div id="nr_ingredients_opcions">
+                        <p id="nr_ingredients_opcions_crear">Crea</p>
+                        <p id="nr_ingredients_opcions_tornar">Torna a la llista</p>
+                        <p id="nr_ingredients_opcions_obrir_menu">Crea un nou ingredient</p>
                      </div>
-                     <div class="nr_tag">
-                        <p>6</p>
-                     </div>
-                     <div class="nr_tag">
-                        <p>8</p>
-                     </div>
-                     <div class="nr_tag">
-                        <p>10</p>
-                     </div>
-                     <p class="nr_input_selection"></p>
+
                   </div>
-               </div>
-               <h3 id="nr_section_ingredients">Ingredients</h3>
-               <h3 id="nr_section_steps">Passos</h3>
 
 
-            </div>
+                  <h3 id="nr_section_steps">Passos</h3>
+
+
+               </div>
+            </form>
          </div>
       </main>
       <?php
